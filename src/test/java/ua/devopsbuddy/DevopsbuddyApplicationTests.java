5c7e21558da91f08170b05c964b83a3560e0a3c3
Ros <ros2306@ukr.net>
2018-03-17 14:57:54 +0200

    add loggin set up

--- src/test/java/ua/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/ua/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -9,21 +9,22 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import ua.devopsbuddy.web.i18n.I18NService;
 
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes=DevopsbuddyApplication.class)
+@ContextConfiguration(classes = DevopsbuddyApplication.class)
 @WebAppConfiguration
 public class DevopsbuddyApplicationTests {
 
-	@Autowired
-	private I18NService i18NService;
+    @Autowired
+    private I18NService i18NService;
 
 
-	@Test
-	public void testMassageByLocaleService()throws Exception {
-		String expectedResult = "This example adds a dropdown menu in the navbar";
-		String messagesId = "index.main.callout";
-		String actual = i18NService.getMessage(messagesId);
-		Assert.assertEquals("The actual and expected Strings don't match", expectedResult, actual);
-	}
+    @Test
+    public void testMassageByLocaleService() throws Exception {
+        String expectedResult = "This example adds a dropdown menu in the navbar";
+        String messagesId = "index.main.callout";
+        String actual = i18NService.getMessage(messagesId);
+        Assert.assertEquals("The actual and expected Strings don't match", expectedResult, actual);
+    }
 
 }
