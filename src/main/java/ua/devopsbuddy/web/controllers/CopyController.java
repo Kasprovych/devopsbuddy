5049525a9a92d279ee266a13b5d79e4f2294d615
Ros <ros2306@ukr.net>
2018-03-17 16:33:28 +0200

    Added about page

---
+++ src/main/java/ua/devopsbuddy/web/controllers/CopyController.java
@@ -0,0 +1,16 @@
+package ua.devopsbuddy.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Rostyslav Kasprovych
+ */
+@Controller
+public class CopyController {
+
+    @RequestMapping("/about")
+    public String about(){
+        return "copy/about";
+    }
+}
