d8515acd1ab0fd3fa8190c81b3636822b7891fd1
Ros <ros2306@ukr.net>
2018-03-17 14:03:22 +0200

    added i18n capability

--- src/test/java/ua/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/ua/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,16 +1,29 @@
 package ua.devopsbuddy;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import ua.devopsbuddy.web.i18n.I18NService;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=DevopsbuddyApplication.class)
+@WebAppConfiguration
 public class DevopsbuddyApplicationTests {
 
+	@Autowired
+	private I18NService i18NService;
+
+
 	@Test
-	public void contextLoads() {
+	public void testMassageByLocaleService()throws Exception {
+		String expectedResult = "This example adds a dropdown menu in the navbar";
+		String messagesId = "index.main.callout";
+		String actual = i18NService.getMessage(messagesId);
+		Assert.assertEquals("The actual and expected Strings don't match", expectedResult, actual);
 	}
 
 }
