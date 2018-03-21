950daf725706c03ce3060c2731047c2cbf808dba
Ros <ros2306@ukr.net>
2018-03-21 17:39:08 +0200

    Added payload page

---
+++ src/main/java/ua/devopsbuddy/web/controllers/PayloadController.java
@@ -0,0 +1,20 @@
+
+package ua.devopsbuddy.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * @author Rostyslav Kasprovych
+ */
+
+@Controller
+public class PayloadController {
+
+    @RequestMapping("/payload")
+    public String payload(){
+        return "payload/payload";
+    }
+}
+
